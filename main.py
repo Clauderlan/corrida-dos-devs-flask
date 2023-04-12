d3511d8297b038dce9869258a58bcd53ed2b44ae
c7 <89857887+Clauderlan@users.noreply.github.com>
2023-04-11 22:50:08 -0300

    ChallengeContentList added and SQL logic

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 import bcrypt
 from flask_cors import CORS
 from JWT import verify_and_decode_jwt, iniciandoJWT
+
 #Precisa instalar os 4 pacotes:
 #Flask
 #Flask-SQLAlchemy
@@ -24,7 +25,9 @@ class Users(Resource):
 
   def get(self):  # Mostra todos os usuários cadastrados no BD
     conn = db_connect.connect()
-    query = conn.execute("select id, userName, userbio, useremail, userrankpoints from user order by userrankpoints desc")
+    query = conn.execute(
+      "select id, userName, userbio, useremail, userrankpoints from user order by userrankpoints desc limit 10"
+    )
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
     challengeContent = ChallengeContentByUserId()
     categoryRule = CategoryByUserId()
@@ -56,7 +59,8 @@ class Users(Resource):
     print()
     return jsonify(result)
 
-  def patch(self):  # Patch para atualizar determinado atributo passado pelo request.
+  def patch(
+      self):  # Patch para atualizar determinado atributo passado pelo request.
 
     conn = db_connect.connect()
     userId = request.json['userId']
@@ -65,8 +69,7 @@ class Users(Resource):
     conn.execute("update user set {0} = '{1}' where id = '{2}'".format(
       patchColumn, valueColumn, userId))
     query = conn.execute(
-      "select id, userName, userbio, useremail, userrankpoints from user where id= '%d' "
-      % int(userId))
+      "select id, userName, userbio, useremail, userrankpoints from user where id= '%d' " % int(userId))
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
     return jsonify(result)
 
@@ -105,21 +108,25 @@ class SocialNetwork(Resource):
     conn = db_connect.connect()
     socialName = request.json['socialName']
     userId = request.json['userId']
-    conn.execute("insert into socialNetwork values(null, '{0}','{1}')".format(socialName, userId))
+    conn.execute("insert into socialNetwork values(null, '{0}','{1}')".format(
+      socialName, userId))
     query = conn.execute('select * from socialNetwork')
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
     return jsonify(result)
 
-
-  def patch(self):  # Patch para atualizar determinado atributo passado pelo request.
+  def patch(
+      self):  # Patch para atualizar determinado atributo passado pelo request.
 
     conn = db_connect.connect()
     oldSocialName = request.json['oldSocialName']
     userId = request.json['userId']
     patchColumn = request.json['patchColumn']
     valueColumn = request.json['valueColumn']
-    conn.execute("update socialNetwork set {0} = '{1}' where socialName = '{2}' and userId = '{3}'".format(patchColumn, valueColumn, oldSocialName, userId))
-    query = conn.execute("select * from socialNetwork where userId= '%d' " % int(userId))
+    conn.execute(
+      "update socialNetwork set {0} = '{1}' where socialName = '{2}' and userId = '{3}'"
+      .format(patchColumn, valueColumn, oldSocialName, userId))
+    query = conn.execute("select * from socialNetwork where userId= '%d' " %
+                         int(userId))
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
     return jsonify(result)
 
@@ -221,7 +228,8 @@ class MaterialContent(Resource):
   def post(self):
     conn = db_connect.connect()
     materialContent = request.json['materialContent']
-    conn.execute("insert into materialContentList values(null, '{0}')".format(materialContent))
+    materialId = request.json['materialId']
+    conn.execute("insert into materialContentList values(null, '{0}', '{1}')".format(materialContent, materialId))
     query = conn.execute('select * from materialContentList order by id desc limit 1')
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
     return jsonify(result)
@@ -230,12 +238,11 @@ class MaterialContent(Resource):
     conn = db_connect.connect()
     materialContentId = request.json['materialContentId']
     materialContent = request.json['materialContent']
+    materialId = request.json['materialId']
     conn.execute(
-      "update materialContentList set materialContent = '{0}' where id = '{1}'"
-      .format(materialContent, str(materialContentId)))
+      "update materialContentList set materialContent = '{0}', materialId = '{1}' where id = '{2}'".format(materialContent, materialId, str(materialContentId)))
     query = conn.execute(
-      "select * from materialContentList where id = '{0}'".format(
-        str(materialContentId)))
+      "select * from materialContentList where id = '{0}'".format(str(materialContentId)))
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
     return jsonify(result)
 
@@ -246,6 +253,9 @@ class Challenges(Resource):
     conn = db_connect.connect()
     query = conn.execute("select * from challenge")
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+    ChallengeContent = ChallengeContentById()
+    for x in result:
+      x["challengeContentList"] = ChallengeContent.get(x["id"])
     return jsonify(result)
 
   def post(self):
@@ -302,6 +312,34 @@ class Challenges(Resource):
 
     query = conn.execute("select * from challenge where id= '%d' " %
                          int(challengeId))
+    result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+    return jsonify(result)
+
+
+class ChallengeContent(Resource):
+
+  def get(self):
+    conn = db_connect.connect()
+    query = conn.execute("select * from challengeContentList")
+    result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+    return jsonify(result)
+
+  def post(self):
+    conn = db_connect.connect()
+    challengeContent = request.json['challengeContent']
+    challengeId = request.json['challengeId']
+    conn.execute("insert into challengeContentList values(null, '{0}','{1}')".format(challengeContent, challengeId))
+    query = conn.execute('select * from challengeContentList order by id desc limit 1')
+    result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+    return jsonify(result)
+
+  def put(self):  # Patch para atualizar determinado atributo passado pelo request.
+    conn = db_connect.connect()
+    challengeContentId = request.json['challengeContentId']
+    challengeContent = request.json['challengeContent']
+    challengeId = request.json['challengeId']
+    conn.execute("update challengeContentList set challengeContent = '{0}', challengeId = '{1}' where id = '{2}'".format(challengeContent, challengeId, str(challengeContentId)))
+    query = conn.execute("select * from challengeContentList where id = '{0}'".format(str(challengeContentId)))
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
     return jsonify(result)
 
@@ -373,13 +411,13 @@ class UserById(Resource):
     conn = db_connect.connect()
     query = conn.execute("select * from user where id = '%d' " % int(id))
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
-    if(not(result)): # Caso o usuário no id não exista
-        return {"message" : "no users"}, 402
-      
+    if (not (result)):  # Caso o usuário no id não exista
+      return {"message": "no users"}, 402
+
     challengeContent = ChallengeContentByUserId()
     categoryRule = CategoryByUserId()
     socialName = SocialByUserId()
-    result = result[0] # Tirando o resultado da lista.
+    result = result[0]  # Tirando o resultado da lista.
     result["userChallengesResponse"] = challengeContent.get(result["id"])
     result["userRule"] = categoryRule.get(result["id"])
     result["socialName"] = socialName.get(result["id"])
@@ -390,15 +428,16 @@ class UserByLogin(Resource):
 
   def get(self, login):  # Busca no BD um usuário passado como parâmetro
     conn = db_connect.connect()
-    query = conn.execute('select * from user where userName = "%s"' % str(login))
+    query = conn.execute('select * from user where userName = "%s"' %
+                         str(login))
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
-    if(not(result)): # Caso o usuário no id não exista
-        return []
-      
+    if (not (result)):  # Caso o usuário no id não exista
+      return []
+
     challengeContent = ChallengeContentByUserId()
     categoryRule = CategoryByUserId()
     socialName = SocialByUserId()
-    result = result[0] # Tirando o resultado da lista.
+    result = result[0]  # Tirando o resultado da lista.
     result["userChallengesResponse"] = challengeContent.get(result["id"])
     result["userRule"] = categoryRule.get(result["id"])
     result["socialName"] = socialName.get(result["id"])
@@ -448,8 +487,16 @@ class MaterialContentById(Resource):
 
   def get(self, id):
     conn = db_connect.connect()
-    query = conn.execute('select * from materialContentList where id = "%d"' %
-                         int(id))
+    query = conn.execute('select materialContent from materialContentList where materialId = "%d"' % int(id))
+    result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+    return result
+
+
+class ChallengeContentById(Resource):
+
+  def get(self, id):
+    conn = db_connect.connect()
+    query = conn.execute('select challengeContent from challengeContentList where challengeId = "%d"' % int(id))
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
     return result
 
@@ -463,9 +510,7 @@ class ChallengeContentByUserId(Resource):
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
     listChallenges = []
     for x in result:
-      queryC = conn.execute(
-        'select challengeTitle from challenge where id = "%d"' %
-        int(x["challengeId"]))
+      queryC = conn.execute('select challengeTitle from challenge where id = "%d"' % int(x["challengeId"]))
       resultC = [dict(zip(tuple(queryC.keys()), i)) for i in queryC.cursor]
       listChallenges.append(resultC[0]["challengeTitle"])
     return listChallenges
@@ -485,7 +530,9 @@ class SocialByUserId(Resource):
 
   def get(self, userId):
     conn = db_connect.connect()
-    query = conn.execute("select socialName from socialNetwork where userId = '%d' " % int(userId))
+    query = conn.execute(
+      "select socialName from socialNetwork where userId = '%d' " %
+      int(userId))
     result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
     return result
 
@@ -495,7 +542,7 @@ def var_user():
   user = request.json  # LOGIN PASSWORD
   byLogin = UserByLogin()  # -> Pegar login e senha, para verificar.
   userByLogin = byLogin.get(user["login"])
-  if (not(userByLogin)):  # Verificando se esse login existe na base de dados.
+  if (not (userByLogin)):  # Verificando se esse login existe na base de dados.
     return {"message": "Crendenciais inválidas"}
   hashed = userByLogin["userPassword"]  # Password do banco
   password = user["password"]  # Password da requisição
@@ -507,7 +554,8 @@ def var_user():
     jwt = iniciandoJWT(userId, roleById.get(userId))  # userId, Role
     return {"JWT": jwt}, 202
   else:
-    return {"message": "Crendenciais inválidas"}, 403
+    return {"message": "Crendenciais inválidas"}
+
 
 api.add_resource(Users, '/users')
 api.add_resource(UserById, '/users/<id>')
@@ -527,7 +575,9 @@ api.add_resource(MaterialContentById, '/materialcontent/<id>')
 
 api.add_resource(Challenges, '/challenges')
 api.add_resource(ChallengesById, '/challenges/<id>')
+api.add_resource(ChallengeContent, '/challengescontent')
 api.add_resource(ChallengeResponse, '/challengesresponse')
+api.add_resource(ChallengeContentById, '/challengesresponse/<id>')
 api.add_resource(ChallengeContentByUserId, '/challengesresponse/<userId>')
 
 if __name__ == '__main__':
